761b0834aec4655e2ffaa637bb305eb1c6b39973
chandratand <Chandra.tand@gmail.com>
2023-04-09 15:12:26 +0700

    Initial setup Next tailwind eslint ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto bg-sky-200">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+      similique doloremque eaque laboriosam minima, repudiandae totam ea
+      incidunt sequi perferendis omnis dolores unde minus nesciunt. Illo culpa
+      numquam itaque!
+    </div>
+  );
+};
+
+export default Home;
